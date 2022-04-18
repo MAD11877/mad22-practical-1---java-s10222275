aa189d09cfbb860cd9a7ab9325307e805f6b9b57
s10222275 <98312402+s10222275@users.noreply.github.com>
2022-04-18 19:55:46 +0800

    Finish question 3

--- Question3.java
+++ Question3.java
@@ -11,6 +11,9 @@ public class Question3
      */
      
     Scanner in = new Scanner(System.in);
+    int num = in.nextInt();
+	  int multply=num*num;
+	  System.out.println(multply);
     
   }
 }
