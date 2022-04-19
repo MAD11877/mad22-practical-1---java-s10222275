67136d5f0c1adab8aea0bc77ffaaaf58486eab32
s10222275 <98312402+s10222275@users.noreply.github.com>
2022-04-19 21:40:33 +0800

    Finish Question 5

--- Question5.java
+++ Question5.java
@@ -25,8 +25,44 @@ public class Question5
      *     2
      * Hint: Use a loop to get input. Use another 2 loops to find the mode
      */
-     
-    Scanner in = new Scanner(System.in);
-    
+    System.out.println("Enter quantity of numbers: "); 
+	    Scanner in = new Scanner(System.in);
+	    int num = in.nextInt();
+	    List<Integer> l1 = new ArrayList<Integer>();//import list
+	    List<Integer> l2 = new ArrayList<Integer>();//import list
+	    List<Integer> l3 = new ArrayList<Integer>();//import list
+	    l2=l1;
+
+	    for (int i = 0;i<num;i++)
+	      {
+	        int num2 = in.nextInt();
+	        l1.add(num2);
+	        
+	      }
+	    for(int k = 0; k<l1.size();k++){
+	    	int snum = l1.get(k);
+	    	int count=0;
+	    	for (int q=0;q<l1.size();q++) {
+	    		if(snum==l1.get(q)) {
+	    			count++;
+
+	    		}
+	    		
+	    	
+	    	}
+
+	    	l3.add(count);
+	    }
+	    int maxnumber= Collections.max(l3);
+	    int pos =0;
+	    for(int h : l3) {
+	    	if (h==maxnumber) {
+	    		pos++;
+	    		break;
+	    	}
+	    }
+	    int ans = l1.get(pos);
+	    System.out.println(ans);
+	    
   }
 }
